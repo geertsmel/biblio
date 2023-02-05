b89a71b564088839834a9a3506fdd2350f51dd9c
Melanie Geerts <geerts.m@hotmail.com>
2023-02-05 19:16:12 +0100

    backup

--- views/backup/voir.php
+++ views/backup/voir.php
@@ -5,3 +5,10 @@
 <p>Remonter avec la flèche vers le haut jusqu'à :</p>
 <code>git add .</code>
 <p>Appuyer sur Enter</p>
+<p>Remonter avec la flèche vers le haut jusqu'à :</p>
+<code>git commit -m "backup"</code>
+<p>Appuyer sur Enter</p>
+<p>Remonter avec la flèche vers le haut jusqu'à : </p>
+<code>git push -u origin main</code>
+<p>Appuyer sur Enter</p>
+<p>Et voilà :D</p>
