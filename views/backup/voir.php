dcb2d12dd4b867635970cc4764298bd97efe953a
Melanie Geerts <geerts.m@hotmail.com>
2023-02-05 19:23:40 +0100

    backup

--- views/backup/voir.php
+++ views/backup/voir.php
@@ -1,14 +1,19 @@
 <h2>Backup</h2>
-<p>1) <a href="http://localhost/phpmyadmin/index.php?route=/database/export&db=biblio" target="_blank">Aller ici</a> et cliquer sur "Exporter"</p>
-<p>Enregistrer le fichier dans c:/xampp/htdocs/Bibliotheque/models</p>
-<p>2) Dans le programme Visual Studio Code, dans le terminal en bas : </p>
-<p>Remonter avec la flèche vers le haut jusqu'à :</p>
-<code>git add .</code>
-<p>Appuyer sur Enter</p>
-<p>Remonter avec la flèche vers le haut jusqu'à :</p>
-<code>git commit -m "backup"</code>
-<p>Appuyer sur Enter</p>
-<p>Remonter avec la flèche vers le haut jusqu'à : </p>
-<code>git push -u origin main</code>
-<p>Appuyer sur Enter</p>
-<p>Et voilà :D</p>
+<div class="container">
+    <div class="flex col">
+        <p>1) <a href="http://localhost/phpmyadmin/index.php?route=/database/export&db=biblio" target="_blank">Aller ici</a> et cliquer sur "Exporter"</p>
+        <p>Enregistrer le fichier dans c:/xampp/htdocs/Bibliotheque/models</p>
+        <p>2) Dans le programme Visual Studio Code, dans le terminal en bas : </p>
+        <p>Remonter avec la flèche vers le haut jusqu'à :</p>
+        <code>git add .</code>
+        <p>Appuyer sur Enter</p>
+        <p>Remonter avec la flèche vers le haut jusqu'à :</p>
+        <code>git commit -m "backup"</code>
+        <p>Appuyer sur Enter</p>
+        <p>Remonter avec la flèche vers le haut jusqu'à : </p>
+        <code>git push -u origin main</code>
+        <p>Appuyer sur Enter</p>
+        <p>Et voilà :D</p>
+    </div>
+
+</div>
